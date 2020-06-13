31b73814363e1dd44d2397d56b7b73361ce885a3
Kavan Samra <kssamra@ucsc.edu>
2020-06-12 22:00:43 -0700

    project title

--- src/styles/templates/project_layout.js
+++ src/styles/templates/project_layout.js
@@ -2,23 +2,39 @@ import styled from 'styled-components';
 import React from 'react';
 import Layout from '../../components/layout';
 import { graphql } from 'gatsby';
+import { Section } from '../blocks/sections/section';
+import { breakpoints } from '../theme';
 /**
  * Layout for detailed project pages
  */
 
- export default function Project({ data }) {
-  console.log(data, null, 4);
-  const project = data.projectsJson;
-  
-   return (
-     <Layout>
-       <h1>{project.title}</h1>
-       <p>{project.desc}</p>
-       <a>{project.github}</a>
+export default function Project({ data }) {
+console.log(data, null, 4);
+const project = data.projectsJson;
 
-     </Layout>
-   );
- };
+  return (
+    <Layout>
+      <Section>
+        <Title>{project.title}</Title>
+        <p>{project.desc}</p>
+        <a>{project.github}</a>
+      </Section>
+    </Layout>
+  );
+};
+
+
+
+const Title = styled.h1`
+  color: white;
+  font-size: 3rem;
+
+  @media ${breakpoints.lg} {
+    font-size: 4rem;
+  }
+`;
+
+
 
 
  export const query = graphql`
