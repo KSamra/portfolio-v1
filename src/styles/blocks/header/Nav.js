d8a3ba8d54c3240c2aeaa75a8f3fafd109351fec
Kavan Samra <kavansamra@gmail.com>
2020-06-20 17:00:44 -0700

    updated resume file

--- src/styles/blocks/header/Nav.js
+++ src/styles/blocks/header/Nav.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 import { ListLink } from "../../elements/Links"
 import React from "react"
-import Resume from "../../../../content/test.pdf"
+import Resume from "../../../../content/resume.pdf"
 import { colors } from "../../theme"
 
 const NavWrapper = styled.nav`
--- src/styles/blocks/mobileHeader/Menu.js
+++ src/styles/blocks/mobileHeader/Menu.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 import React from "react"
 import { Link } from "gatsby"
-import Resume from "../../../../content/test.pdf"
+import Resume from "../../../../content/resume.pdf"
 import { colors } from "../../theme"
 
 const Menu = ({ isOpen, clickHandler }) => {
